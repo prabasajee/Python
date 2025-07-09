cb76852c17b9f2b21bd73f8ee96ad4361c45b6c9
prabasajee <prabaharansajeevan23@gmail.com>
2025-07-09 23:22:01 +0530

    new files for pratices

--- MISC/inputfile.py
+++
@@ -1,6 +0,0 @@
-inputfile= open("inputFile.txt", "r")
-for line in inputfile:
-    line = line.strip()
-    if line[2]== "c":
-        print(line)
-inputfile.close()
--- MISC/pratice.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-for i in range(5):
-    subprocess.check_call(["python", "example.py"])
