7b74e10f0f1eac503b7c6b258d6c509914b9e906
prabasajee <prabaharansajeevan23@gmail.com>
2025-07-09 22:56:29 +0530

    update code

--- inputfile.py
+++ inputfile.py
@@ -1,6 +1,6 @@
-inputfile= open("input.txt", "r")
+inputfile= open("inputFile.txt", "r")
 for line in inputfile:
     line = line.strip()
-    if line[2]== 'P':
+    if line[2]== "c":
         print(line)
 inputfile.close()
